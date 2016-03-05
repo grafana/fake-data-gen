e02428a3e675102ab57fdc309670856c220b7a04
Torkel Ödegaard <torkel.odegaard@gmail.com>
2016-03-05 18:32:31 +0100

    updated

--- prom_data.js
+++ prom_data.js
@@ -4,24 +4,34 @@ var Prometheus = require("prometheus-client");
 function live() {
   var client = new Prometheus();
 
-  var counters = client.newCounter({
+  var logins = client.newCounter({
     namespace: "counters",
     name: "logins",
     help: "Counters"
   });
 
+  var requests = client.newCounter({
+    namespace: "counters",
+    name: "requests",
+    help: "Counters"
+  });
+
+
   client.listen(9091);
   var data = {};
 
-  function randomWalk(counter, labels, variation) {
-    counter.increment(labels, (Math.random() * variation) - (variation / 2));
+  function randomWalk(labels, variation) {
+    logins.increment(labels, (Math.random() * variation) - (variation / 2));
+    requests.increment(labels, 100 + (Math.random() * 10));
   }
 
   setInterval(function() {
-    randomWalk(counters, {server: "backend-01", app: "backend"}, 2);
-    randomWalk(counters, {server: "backend-02", app: "backend"}, 2);
-    randomWalk(counters, {server: "webserver-01", app: "frontend"}, 2);
-    randomWalk(counters, {server: "webserver-02", app: "frontend"}, 2);
+    randomWalk({server: "backend-01", app: "backend"}, 2);
+    randomWalk({server: "backend-02", app: "backend"}, 2);
+    randomWalk({server: "webserver-01", app: "frontend"}, 2);
+    randomWalk({server: "webserver-02", app: "frontend"}, 2);
+    randomWalk({server: "webserver_03", app: "frontend"}, 2);
+    randomWalk({server: "webserver.03", app: "frontend"}, 2);
   }, 10000);
 }
 
