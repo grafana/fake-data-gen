dfc06642da5dd0e24448b4256045436b216707ee
Jim Nagy <jnagy@grafana.com>
2018-01-25 11:53:31 -0500

    Adding the ability to set env var FD_SERVER to choose where to send fake data

--- src/start.sh
+++ src/start.sh
@@ -6,10 +6,12 @@ echo $hostip
 
 : "${FD_DATASOURCE:=graphite}"
 : "${FD_PORT:=2003}"
+: "${FD_SERVER:=$hostip}"
 
 echo $FD_DATASOURCE
 echo $FD_PORT
+echo $FD_SERVER
 
-params="--${FD_DATASOURCE} --server ${hostip} --port ${FD_PORT} --live --import --days 3"
+params="--${FD_DATASOURCE} --server ${FD_SERVER} --port ${FD_PORT} --live --import --days 3"
 echo $params
 node app.js $params
