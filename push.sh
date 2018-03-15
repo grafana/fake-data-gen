4169e96835e19eea0df0ae08b934c1e89f38898e
Marcus Efraimsson <marcus.efraimsson@gmail.com>
2018-03-15 09:43:28 +0100

    fix push

--- push.sh
+++ push.sh
@@ -3,6 +3,5 @@ VERSION=v3
 
 docker build -t grafana/fake-data-gen:latest -t grafana/fake-data-gen:$VERSION --no-cache=true .
 
-docker push grafana/fake-data-gen
 docker push grafana/fake-data-gen:latest
 docker push grafana/fake-data-gen:$VERSION
