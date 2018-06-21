ad9f6f10ded8b5b3cf91f326bde00d20912badf6
Daniel Lee <dan.limerick@gmail.com>
2018-06-21 23:18:50 +0200

    add latitude/longitude to Influx fake data

--- src/influx_data.js
+++ src/influx_data.js
@@ -122,21 +122,21 @@ function live(server, port) {
   }
 
   setInterval(function() {
-    randomWalk('logins.count', { source: 'backend', hostname: '10.1.100.1', datacenter: "America", geohash: "9wvfgzurfzb" }, 100, 2);
-    randomWalk('logins.count', { source: 'backend', hostname: '10.1.100.10', datacenter: "America", geohash: "dre33fzyxcrz" }, 100, 2);
-    randomWalk('logins.count', { source: 'backend', hostname: 'server1', datacenter: "America", geohash: "dr199bpvpcru" }, 100, 2);
-    randomWalk('logins.count', { source: 'backend', hostname: 'server2', datacenter: "America", geohash: "9yy21uzzxypg"}, 100, 2);
-    randomWalk('logins.count', { source: 'backend', hostname: 'server3', datacenter: "Europe", geohash: "gc6j7crvrcpf"}, 100, 2);
-    randomWalk('logins.count', { source: 'backend', hostname: 'asd/ space/ metric', datacenter: "Europe", geohash: "u6g9zuxvxypv"}, 100, 2);
-    randomWalk('logins.count', { source: 'backend', hostname: 'asd2\\ asd\\1', datacenter: "Europe", geohash: "gbsuv7ztq111"}, 100, 2);
-    randomWalk('logins.count', { source: 'backend', hostname: 'server4', datacenter: "Europe", geohash: "gcp03b022111"}, 100, 2);
-    randomWalk('logins.count', { source: 'backend', hostname: 'server\\5', datacenter: "Asia", geohash: "tgcpegxbpbzu"}, 100, 2);
-    randomWalk('logins.count', { source: 'backend', hostname: 'server/7', datacenter: "Africa", geohash: "kd3ezvxvxypc"}, 100, 2);
-    randomWalk('logins.count', { source: 'site', hostname: 'server1', datacenter: "America", geohash: "tz4vytepg111" }, 100, 2);
-    randomWalk('logins.count', { source: 'site', hostname: 'server2', datacenter: "America", geohash: "wjms0jyr5111" }, 100, 2);
-    randomWalk('cpu', { source: 'site', hostname: 'server1', datacenter: "America", geohash: "wj7c61wnv111" }, 100, 2);
-    randomWalk('cpu', { source: 'site', hostname: 'server2', datacenter: "America", geohash: "tz6h548nc111" }, 100, 2);
-    randomWalk('cpu', { source: 'site', hostname: 'server2', datacenter: "America", geohash: "wr50zpuhj111" }, 100, 2);
+    randomWalk('logins.count', { source: 'backend', hostname: '10.1.100.1', datacenter: "America", geohash: "9wvfgzurfzb", latitude: 42.1497, longitude: -74.9384 }, 100, 2);
+    randomWalk('logins.count', { source: 'backend', hostname: '10.1.100.10', datacenter: "America", geohash: "dre33fzyxcrz", latitude: 38.4199, longitude: -117.1219 }, 100, 2);
+    randomWalk('logins.count', { source: 'backend', hostname: 'server1', datacenter: "America", geohash: "dr199bpvpcru", latitude: 40.3736, longitude: -82.7755 }, 100, 2);
+    randomWalk('logins.count', { source: 'backend', hostname: 'server2', datacenter: "America", geohash: "9yy21uzzxypg", latitude: 47.3917, longitude: -121.5708}, 100, 2);
+    randomWalk('logins.count', { source: 'backend', hostname: 'server3', datacenter: "Europe", geohash: "gc6j7crvrcpf", latitude: 46.151241, longitude: 14.995463}, 100, 2);
+    randomWalk('logins.count', { source: 'backend', hostname: 'asd/ space/ metric', datacenter: "Europe", geohash: "u6g9zuxvxypv", latitude: 60.128161, longitude: 18.643501}, 100, 2);
+    randomWalk('logins.count', { source: 'backend', hostname: 'asd2\\ asd\\1', datacenter: "Europe", geohash: "gbsuv7ztq111", latitude: 53.41291, longitude: -8.24389}, 100, 2);
+    randomWalk('logins.count', { source: 'backend', hostname: 'server4', datacenter: "Europe", geohash: "gcp03b022111", latitude: 45.1, longitude: 15.2}, 100, 2);
+    randomWalk('logins.count', { source: 'backend', hostname: 'server\\5', datacenter: "Asia", geohash: "tgcpegxbpbzu", latitude: 22.396428, longitude: 114.109497}, 100, 2);
+    randomWalk('logins.count', { source: 'backend', hostname: 'server/7', datacenter: "Africa", geohash: "kd3ezvxvxypc", latitude: -4.038333, longitude: 21.758664}, 100, 2);
+    randomWalk('logins.count', { source: 'site', hostname: 'server1', datacenter: "America", geohash: "tz4vytepg111", latitude: 36.1700, longitude: -119.7462 }, 100, 2);
+    randomWalk('logins.count', { source: 'site', hostname: 'server2', datacenter: "America", geohash: "wjms0jyr5111", latitude: 61.3850, longitude: -152.2683 }, 100, 2);
+    randomWalk('cpu', { source: 'site', hostname: 'server1', datacenter: "America", geohash: "wj7c61wnv111", latitude: 44.2563, longitude: -89.6385 }, 100, 2);
+    randomWalk('cpu', { source: 'site', hostname: 'server2', datacenter: "America", geohash: "tz6h548nc111", latitude: 27.8333, longitude: -81.7170 }, 100, 2);
+    randomWalk('cpu', { source: 'site', hostname: 'server2', datacenter: "America", geohash: "wr50zpuhj111", latitude: 38.8964, longitude: -77.0262 }, 100, 2);
     randomWalk('payment.started', { source: 'frontend', hostname: 'server2', datacenter: "America" }, 1000, 5);
     randomWalk('payment.started', { source: 'frontend', hostname: 'server1', datacenter: "America"  }, 1000, 5);
     randomWalk('payment.ended', { source: 'frontend', hostname: 'server1', datacenter: "America"  }, 1000, 5);
