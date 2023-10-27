b7d0a3ac329bd2a9d023469abdf8fbba64ac7994
Galen <galen.kistler@grafana.com>
2023-10-27 14:23:09 -0500

    updating prom client

--- src/prom_data.js
+++ src/prom_data.js
@@ -1,22 +1,19 @@
-var client = require("prom-client")
+const client = require("prom-client")
 const express = require('express');
-const cluster = require('cluster');
 const server = express();
 const register = client.register;
 
-// Enable collection of default metrics
-
-client.collectDefaultMetrics({
-  gcDurationBuckets: [0.001, 0.01, 0.1, 1, 2, 5], // These are the default buckets.
-});
+/**
+ * Adapting the prom-client example to work with gdev-prometheus
+ * https://github.com/siimon/prom-client/blob/master/example/server.js
+ */
 
 // Create custom metrics
-
 const Histogram = client.Histogram;
 const h = new Histogram({
   name: 'test_histogram',
   help: 'Example of a histogram',
-  labelNames: ['code'],
+  labelNames: ['code', 'color'],
 });
 
 const Counter = client.Counter;
@@ -27,7 +24,7 @@ const c = new Counter({
 });
 
 new Counter({
-  name: 'scrape_counter',
+  name: 'fake_data_gen_scrapes_total',
   help: 'Number of scrapes (example of a counter with a collect fn)',
   collect() {
     // collect is invoked each time `register.metrics()` is called.
@@ -42,49 +39,40 @@ const g = new Gauge({
   labelNames: ['method', 'code'],
 });
 
+const work = () => {
 // Set metric values to some random values for demonstration
+  setTimeout(() => {
+    h.labels('200', 'blue').observe(Math.floor(Math.random() * 200));
+    h.labels('300', 'red').observe(Math.random());
+    h.labels('300', 'blue').observe(Math.random());
+    h.labels('200', 'red').observe(1 - (Math.random() / 10));
 
-setTimeout(() => {
-  h.labels('200').observe(Math.random());
-  h.labels('300').observe(Math.random());
-}, 10);
+    h.observe(Math.random());
+  }, 999);
 
-setInterval(() => {
-  c.inc({ code: 200 });
-}, 5000);
-
-setInterval(() => {
-  c.inc({ code: 400 });
-}, 2000);
-
-setInterval(() => {
-  c.inc();
-}, 2000);
-
-setInterval(() => {
-  g.set({ method: 'get', code: 200 }, Math.random());
-  g.set(Math.random());
-  g.labels('post', '300').inc();
-}, 100);
-
-if (cluster.isWorker) {
-  // Expose some worker-specific metric as an example
   setInterval(() => {
-    c.inc({ code: `worker_${cluster.worker.id}` });
-  }, 2000);
-}
+    c.inc({ code: Math.random() > 0.99 ? 500 : 200 });
+  }, 5000);
 
-const t = [];
-setInterval(() => {
-  for (let i = 0; i < 100; i++) {
-    t.push(new Date());
-  }
-}, 10);
-setInterval(() => {
-  while (t.length > 0) {
-    t.pop();
-  }
-});
+  setInterval(() => {
+    c.inc({ code: Math.random() < 0.07 ? 400 : Math.random() > 0.05 ? 414 : 200 })
+  }, 2000);
+
+  setInterval(() => {
+    c.inc({code: 200});
+  }, 2000);
+
+  setInterval(() => {
+    g.set({ method: 'get', code: Math.random() > 0.95 ? 500 : 200 }, Math.random());
+    g.set(Math.random());
+    g.labels('post', '300').inc();
+  }, 100);
+
+  client.collectDefaultMetrics({
+    timeout: 10000,
+    gcDurationBuckets: [0.001, 0.01, 0.1, 1, 2, 5] // These are the default buckets.
+  });
+}
 
 // Setup server to Prometheus scrapes:
 server.get('/metrics', async (req, res) => {
@@ -114,5 +102,6 @@ module.exports = {
         `Server listening to ${port}, metrics exposed on /metrics endpoint`,
     );
     server.listen(port);
+    work();
   }
 };
